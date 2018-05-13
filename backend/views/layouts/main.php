3eaf09897635ea092402b5494dd7d2481ccee45d
Alexandr <grandmasteralexandr@gmail.com>
2018-05-13 20:08:15 +0300

    добавил новые разделы меню сайта

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -37,6 +37,8 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'Rubric', 'url' => ['/rubric/index']],
+        ['label' => 'Post', 'url' => ['/post/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
