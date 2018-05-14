9128378ae8786bea58849b50865b77a5c35735ef
Alexandr <grandmasteralexandr@gmail.com>
2018-05-14 19:55:25 +0300

    миграция на добавление полей creation_time и update_time (type=timestamp) в таблицу post

---
+++ console/migrations/m180514_165134_add_creation_time_column_to_post_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding creation_time to table `post`.
+ */
+class m180514_165134_add_creation_time_column_to_post_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('post', 'creation_time', $this->timestamp());
+        $this->addColumn('post', 'update_time', $this->timestamp());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('post', 'creation_time');
+        $this->dropColumn('post', 'update_time');
+    }
+}
